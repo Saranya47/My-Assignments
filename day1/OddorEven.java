22c2d4c6e57cf1379a174ff055791c35240eb1be
Saranya47 <140278740+Saranya47@users.noreply.github.com>
2023-07-23 01:57:37 +0530

    Add files via upload
    Day1

---
+++ day1/OddorEven.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class OddorEven {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a=25;
+		
+		if(a%2==0) {
+			
+		
+			System.out.println("The number is odd");
+	}
+	else {
+		System.out.println("The number is even");
+	}
+	}
+}
---
+++ day1/PrimitiveDataType.java
@@ -0,0 +1,26 @@
+package week1.day1;
+
+public class PrimitiveDataType {
+
+	public static void main(String[] args) {
+		
+
+		int age=34;
+		float ht=5.4f;
+		double salary=50.000;
+		char initial='S';
+		String name="Saranya";
+	    long PhoneNo=897654321;
+	    boolean Working=false;
+		
+		
+		System.out.println("My age is " +age);
+		System.out.println("Age is " +ht);
+		System.out.println("My current salary is " +salary);
+		System.out.println("My phoneNo is " +PhoneNo);
+		System.out.println("My name is "+initial+"."+name);
+		System.out.println("Currently Working "+ Working);
+
+	}
+
+}
